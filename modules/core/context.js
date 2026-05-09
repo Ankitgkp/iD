8086d3aaa8f834812957f85c249f97323bf2bd84
Tobias <tobias@fixmycity.de>
2026-05-09 22:17:16 +0200

    Revert "refactor: use presets= for merge URL; drop presets_merge"
    This reverts commit 01ed3bca997185bc4b18be9210f711a5413ebf30.

--- modules/core/context.js
+++ modules/core/context.js
@@ -588,11 +588,9 @@ export function coreContext() {
     // Set up objects that might need to access properties of `context`. The order
     // might matter if dependents make calls to each other. Be wary of async calls.
     function initializeDependents() {
-      const presetsHash = context.initialHashParams.presets;
-      const presetsHashIsMergeUrl = presetsHash && /^https?:\/\//i.test(presetsHash.trim());
 
-      if (presetsHash && !presetsHashIsMergeUrl) {
-        presetManager.addablePresetIDs(new Set(presetsHash.split(',')));
+      if (context.initialHashParams.presets) {
+        presetManager.addablePresetIDs(new Set(context.initialHashParams.presets.split(',')));
       }
 
       if (context.initialHashParams.locale) {
@@ -621,8 +619,8 @@ export function coreContext() {
       // Migrate history data from localStorage to IndexedDB
       _history.migrateHistoryData();
 
-      if (presetsHashIsMergeUrl) {
-        presetManager.mergePresets(presetsHash);
+      if (context.initialHashParams.presets_merge) {
+        presetManager.mergePresetsMerge(context.initialHashParams.presets_merge);
       }
 
       if (services.maprules && context.initialHashParams.maprules) {
--- modules/presets/index.js
+++ modules/presets/index.js
@@ -197,21 +197,21 @@ export function presetIndex() {
 
   /**
    * Loads custom preset JSON from a remote location (value of the
-   * `#presets=` hash parameter when it is an `http`/`https` URL) after bundled
-   * presets are loaded, then merges via `merge`. Logs `[presets]` lines to the
+   * `#presets_merge=` hash parameter) after bundled presets are loaded, then
+   * merges via `merge`. Logs `[presets_merge]` lines to the
    * console on success or failure.
    *
-   * @param {string} presetsJsonUrl
+   * @param {string} presetsMergeUrl
    */
-  _this.mergePresets = function (presetsJsonUrl) {
+  _this.mergePresetsMerge = function (presetsMergeUrl) {
     _this.ensureLoaded()
       .then(function () {
-        return d3_json(presetsJsonUrl);
+        return d3_json(presetsMergeUrl);
       })
       .then(function (data) {
         if (!data || typeof data !== 'object' || Array.isArray(data)) {
           console.warn(  // eslint-disable-line no-console
-            '[presets] expected the JSON at ' + presetsJsonUrl +
+            '[presets_merge] expected the JSON at ' + presetsMergeUrl +
             ' to be an object with `presets`/`fields`/`categories`/`defaults` keys, got ' +
             (Array.isArray(data) ? 'array' : typeof data) + '. No presets loaded.'
           );
@@ -219,13 +219,13 @@ export function presetIndex() {
         }
         _this.merge(data);
         console.info(  // eslint-disable-line no-console
-          '[presets] merged custom presets from ' + presetsJsonUrl,
+          '[presets_merge] merged custom presets from ' + presetsMergeUrl,
           data
         );
       })
       .catch(function (err) {
         console.warn(  // eslint-disable-line no-console
-          '[presets] failed to load presets from ' + presetsJsonUrl + ':', err
+          '[presets_merge] failed to load presets from ' + presetsMergeUrl + ':', err
         );
       });
   };
